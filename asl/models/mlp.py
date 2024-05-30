56e9693b514293ff31083b9e5caee9af69c1c0fe
JamorMoussa <moussajamorsup@gmail.com>
2024-05-30 18:25:08 +0100

    comment mlp py file

--- asl/models/mlp.py
+++ asl/models/mlp.py
@@ -1,31 +1,31 @@
-## TODO: create MLPConfigs + MLPModel 
+# ## TODO: create MLPConfigs + MLPModel 
 
-from dataclasses import dataclass
-import torch, torch.nn as nn
+# from dataclasses import dataclass
+# import torch, torch.nn as nn
 
-__all__ = ["MLPConfigs", "MLPModel"]
+# __all__ = ["MLPConfigs", "MLPModel"]
 
-@dataclass
-class MLPConfigs:
+# @dataclass
+# class MLPConfigs:
 
-    input_size: int = 3
-    size: int = 10
-    ld: float = 1.0
+#     input_size: int = 3
+#     size: int = 10
+#     ld: float = 1.0
 
-    @staticmethod
-    def get_defaults(self):
-        return MLPConfigs()
+#     @staticmethod
+#     def get_defaults(self):
+#         return MLPConfigs()
 
 
-class MLPModel(nn.Module):
+# class MLPModel(nn.Module):
 
-    configs: MLPConfigs
+#     configs: MLPConfigs
 
-    def __init__(self, configs: MLPConfigs = MLPConfigs.get_defaults()):
-        self.configs = configs
+#     def __init__(self, configs: MLPConfigs = MLPConfigs.get_defaults()):
+#         self.configs = configs
 
-        nn.Sequential(
-            nn.Linear(self.configs.input_size, )
-        )
+#         nn.Sequential(
+#             nn.Linear(self.configs.input_size, )
+#         )
 
 
