60744f337b31b2839d02eea23db176b245512927
JamorMoussa <moussajamorsup@gmail.com>
2024-05-31 18:55:24 +0100

    add labels to plot

--- asl/plot.py
+++ asl/plot.py
@@ -8,14 +8,24 @@ def plot_base(
     root: str,
     arr1: list,
     arr2: list,
-    fig_name: str
+    fig_name: str,
+    title: str,
+    x_label: str, 
+    y_label: str,
+    plot_label1: str,
+    plot_label2: str,
 ):
     arr1 = np.array(arr1)
     arr2 = np.array(arr2)
 
-    plt.plot(arr1)
-    plt.plot(arr2)
+    plt.plot(arr1, label= plot_label1)
+    plt.plot(arr2, label= plot_label2)
 
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    
+    plt.legend(loc="upper left")
     plt.savefig(osp.join(root, f"{fig_name}.png"))
     plt.clf() 
 
@@ -30,7 +40,12 @@ def plot_loss(
         root= root,
         arr1= train_loss,
         arr2= test_loss,
-        fig_name= "loss_plot"
+        fig_name= "loss_plot",
+        title= "Training and Testing Loss",
+        x_label= "epochs",
+        y_label= "loss",
+        plot_label1= "Training Loss",
+        plot_label2= "Testing Loss",
     )
 
 def plot_accuracy(
@@ -43,5 +58,10 @@ def plot_accuracy(
         root= root,
         arr1= train_acc,
         arr2= test_acc,
-        fig_name= "accuracy_plt"
+        fig_name= "accuracy_plt",
+        title= "Training and Testing Accuracy",
+        x_label= "epochs",
+        y_label= "Accuracy",
+        plot_label1= "Training Accuracy",
+        plot_label2= "Testing Accuracy",
     )
